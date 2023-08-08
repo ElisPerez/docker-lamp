fedbefa4572e0d10fddeb3df41e3f479356a52c2
Elis Antonio Perez <elisperezmusic@gmail.com>
2023-08-08 11:08:26 -0500

    first commit

---
+++ MySQL-AppSalon.session.sql
@@ -0,0 +1,13 @@
+-- Para correr un comando primero sonbrear el comando y luego la conbinacion de teclas CTRL + E CTRL + E (2 veces control + E)
+
+SHOW DATABASES;
+
+CREATE DATABASE IF NOT EXISTS appsalon;
+
+USE appsalon;
+
+SHOW TABLES;
+
+CREATE TABLE IF NOT EXISTS servicios (
+
+);
